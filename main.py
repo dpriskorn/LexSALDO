ae2643903f6d53899a6b990337a6b0b88bd17057
Dennis Priskorn <egils@riseup.net>
2024-05-24 12:48:49 +0200

    feat: Add counts and rename a few attributes.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import logging
 import uuid
-from typing import List, Optional
+from typing import List
 
 import jsonlines
-from pydantic import BaseModel
 from bs4 import BeautifulSoup
+from pydantic import BaseModel
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -53,22 +53,20 @@ class SenseRelation(BaseModel):
 
 class Sense(BaseModel):
     id: str
-    sense_relation: Optional[SenseRelation] = None
+    sense_relations: List[SenseRelation] = []
 
     @classmethod
     def from_soup(cls, soup):
-        sense_relation = soup.find("SenseRelation")
-        return cls(
-            id=soup["id"],
-            sense_relation=SenseRelation.from_soup(sense_relation)
-            if sense_relation
-            else None,
-        )
+        sense_relations = [
+            SenseRelation.from_soup(sr) for sr in soup.find_all("SenseRelation")
+        ]
+        return cls(id=soup["id"], sense_relations=sense_relations)
+
 
 
 class LexicalEntry(BaseModel):
     lemma: Lemma
-    sense: List[Sense]
+    senses: List[Sense]
     # Not all entries have a lemma with a lemgram so
     # lemgram is not a good unique identifier for lexical entries
     lemgram: str  # we want a validation error if no lemgram is found
@@ -80,17 +78,20 @@ class LexicalEntry(BaseModel):
         if not lemma:
             logger.warning(f"no lemma found in soup: {soup}")
         else:
-            lemgram_node = soup.find('Lemma').find('feat', {'att': 'lemgram'})
+            lemgram_node = soup.find("Lemma").find("feat", {"att": "lemgram"})
             if lemgram_node is None:
-                # This seems to be some kind of root node for the senses with no content.
-                logger.warning(f"no lemgram node found in soup: {soup}. Ignoring the entry")
+                # This seems to be some kind of root node
+                # for the senses with no content.
+                logger.warning(
+                    f"no lemgram node found in soup: {soup}. Ignoring the entry"
+                )
                 return None
             else:
-                lemgram = lemgram_node['val']
+                lemgram = lemgram_node["val"]
                 senses = [Sense.from_soup(sense) for sense in soup.find_all("Sense")]
                 return cls(
                     lemma=Lemma.from_soup(lemma) if lemma else None,
-                    sense=senses,
+                    senses=senses,
                     lemgram=lemgram,
                     entry_id=str(uuid.uuid4())[:6],  # Generate unique ID
                 )
@@ -107,7 +108,7 @@ class Lexicon(BaseModel):
             LexicalEntry.from_soup(le) for le in soup.find_all("LexicalEntry")
         ]
         # remove None from the list
-        lexical_entries = [l for l in lexical_entries if l is not None]
+        lexical_entries = [entry for entry in lexical_entries if entry is not None]
         return cls(language=language, lexical_entries=lexical_entries)
 
 
@@ -148,7 +149,7 @@ class LexicalResource(BaseModel):
         """Count number of lexical_entries and add all ids to a set.
         Then verify that the set and count is the same"""
         lexical_entry_ids = [
-            sense.id for entry in self.lexicon.lexical_entries for sense in entry.sense
+            sense.id for entry in self.lexicon.lexical_entries for sense in entry.senses
         ]
         return len(lexical_entry_ids) == len(set(lexical_entry_ids))
 
@@ -159,9 +160,50 @@ class LexicalResource(BaseModel):
             for entry in self.lexicon.lexical_entries:
                 writer.write(entry.model_dump())
 
+    def count_lexical_entries(self):
+        """Count the number of lexical entries."""
+        return len(self.lexicon.lexical_entries)
 
-# Example usage:
+    def count_senses(self):
+        """Count the number of senses."""
+        return sum(len(entry.senses) for entry in self.lexicon.lexical_entries)
+
+    def count_unique_senses(self):
+        """Count the number of unique senses based on Sense.id."""
+        sense_ids = [
+            sense.id for entry in self.lexicon.lexical_entries for sense in entry.senses
+        ]
+        return len(set(sense_ids))
+
+    def count_total_sense_relations(self):
+        """Count the total number of sense relations."""
+        sense_relations = [
+            sense_relation
+            for entry in self.lexicon.lexical_entries
+            for sense in entry.senses
+            for sense_relation in sense.sense_relations
+        ]
+        return len(sense_relations)
+
+    def calculate_avg_sense_relations_per_sense(self):
+        """Calculate the average number of sense relations per sense."""
+        total_senses = sum(len(entry.senses) for entry in self.lexicon.lexical_entries)
+        total_sense_relations = self.count_total_sense_relations()
+
+        if total_senses == 0:
+            return 0
+
+        return round(total_sense_relations / total_senses, 1)
+
+
 file_path = "data/saldo.xml"  # replace with your actual file path
 lexical_resource = LexicalResource.from_file(file_path)
 lexical_resource.output_to_jsonl()
-# print(lexical_resource.json(indent=2))
+print("Total lexical entries:", lexical_resource.count_lexical_entries())
+print("lexical entries with at least one sense:", lexical_resource.count_senses())
+print("Total unique senses:", lexical_resource.count_unique_senses())
+print("Total sense relations:", lexical_resource.count_total_sense_relations())
+print(
+    "Average number of sense relations per sense:",
+    lexical_resource.calculate_avg_sense_relations_per_sense(),
+)
